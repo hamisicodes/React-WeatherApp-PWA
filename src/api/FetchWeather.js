238367b5eecb790f8e4d21bd0e09874b757ea2e0
hamisicodes <newhamisi@gmail.com>
2020-09-29 20:20:54 +0300

    consume API and design layout 💡

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -3,12 +3,15 @@ import axios from 'axios';
 const URL ='https://api.openweathermap.org/data/2.5/weather'
 const API_KEY ='e71424c8e20f5af13dc6c1a9194704f4'
 
+// const URL = 'https://api.openweathermap.org/data/2.5/weather';
+// const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+
 export const fetchWeather = async (query) => {
     const {data} = await axios.get(URL, {
         params:{
             q:query,
             units:'metric',
-            APP_ID:API_KEY,
+            APPID:API_KEY,
         }
     });
 
